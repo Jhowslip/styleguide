47943b69a8a9c739c56ec0d91810467e23c29ffb
Túlio Freitas <tuliofreitas@live.com>
2017-04-16 17:37:36 -0300

    Added show and hide for password inputs

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -11,3 +11,19 @@ $(".sibling-button").focus(function(){
   $(this).parent().addClass("is-active");
   $(this).siblings(".input-on").addClass("button-on");
 });
+
+
+$('.showhide').on('click', function(){
+	if ($('#password').attr('type') == 'password') {
+		$(this).siblings("input").attr('type', 'text');
+		$(this).addClass("icon-hide-password");
+		$(this).removeClass("icon-show-password");
+		$(this).children("p").text("Ver");
+	} else {
+		$(this).siblings("input").attr('type', 'password');
+		$(this).removeClass("icon-hide-password");
+		$(this).addClass("icon-show-password");
+		$(this).children("p").text("Esconder");
+	}
+	
+});
